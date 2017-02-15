0d0ad420a1495b6f045a514795f00ed782214fc0
Jay Ravaliya <jayrav13@gmail.com>
2017-02-15 13:53:26 -0500

    Updating linked files to be relative

--- views/header.php
+++ views/header.php
@@ -5,9 +5,9 @@
     <head>
 
         <!-- http://getbootstrap.com/ -->
-        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="./css/bootstrap.min.css" rel="stylesheet"/>
 
-        <link href="/css/styles.css" rel="stylesheet"/>
+        <link href="./css/styles.css" rel="stylesheet"/>
 
         <?php if (isset($title)): ?>
             <title>C$50 Finance: <?= htmlspecialchars($title) ?></title>
@@ -16,12 +16,12 @@
         <?php endif ?>
 
         <!-- https://jquery.com/ -->
-        <script src="/js/jquery-1.11.3.min.js"></script>
+        <script src="./js/jquery-1.11.3.min.js"></script>
 
         <!-- http://getbootstrap.com/ -->
-        <script src="/js/bootstrap.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
 
-        <script src="/js/scripts.js"></script>
+        <script src="./js/scripts.js"></script>
 
     </head>
 
@@ -31,7 +31,7 @@
 
             <div id="top">
                 <div>
-                    <a href="/"><img alt="C$50 Finance" src="/img/logo.png"/></a>
+                    <a href="/"><img alt="C$50 Finance" src="./img/logo.png"/></a>
                 </div>
                 <?php if (!empty($_SESSION["id"])): ?>
                     <ul class="nav nav-pills">
