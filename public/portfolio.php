5937fee3f545be079e80de92bbe8fba7df8944ef
Jay Ravaliya <jayrav13@gmail.com>
2017-02-20 01:53:06 -0500

    New portfolio buy logic

--- public/portfolio.php
+++ public/portfolio.php
@@ -3,33 +3,108 @@
 	// configuration
 	require("../includes/config.php");
 
+	// Check for GET parameter.
+	if (empty($_GET["id"]))
+	{
+		redirect('./');
+	}
+
+	$portfolio = CS50::query("SELECT * FROM portfolios WHERE id = ? AND user_id = ?", $_GET["id"], $_SESSION["cs673_id"]);
+	if (count($portfolio) != 1)
+	{
+		redirect('./');
+	}
+
+	$portfolio = $portfolio[0];
+	$tickers = CS50::query("SELECT * FROM tickers WHERE portfolio_id = ?", $portfolio["id"]);
+
+	// Static data, can be database'd later.
+	$exchanges = [
+		"nyse" => "New York Stock Exchange",
+		"nasdaq" => "NASDAQ",
+		"nse" => "National Stock Exchange of India Ltd."
+	];
+
+	$output = [
+		"portfolio" => $portfolio,
+		"tickers" => $tickers,
+		"title" => "Holdings",
+		"subtitle" => $portfolio["name"],
+		"exchanges" => $exchanges
+	];
+
 	if ($_SERVER["REQUEST_METHOD"] == "GET")
 	{
-		if (empty($_GET["id"]))
+		render('portfolio.php', $output);
+	}
+	else if ($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+
+		if (empty($_POST["ticker"]) || empty($_POST["shares"]))
+		{
+			$output["errors"] = ['Be sure to provide a stock symbol and number of shares!'];
+			render('portfolio.php', $output);
+		}
+
+		if(!is_numeric($_POST["shares"]))
+		{
+			$output["errors"] = ['The number of shares must be numeric!'];
+			render('portfolio.php', $output);
+		}
+
+		$_POST["ticker"] = strtoupper($_POST["ticker"]);
+		$stock = ticker($_POST["ticker"], $_POST["exchange"] ?: null);
+
+		if( ! $stock )
+		{
+			$output['errors'] = [$_POST["ticker"] . ' in the ' . $exchanges[$_POST["exchange"]] . ' could not be found.'];
+			render('portfolio.php', $output);
+		}
+
+		if ( !empty($_POST["price"]) )
+		{
+			$output["price"] = floatval($_POST["price"]);
+		}
+
+		$cost = $_POST["shares"] * $stock["price"];
+		$user = CS50::query("SELECT * FROM users WHERE id = ?", $_SESSION["cs673_id"]);
+
+		if(count($user) != 1)
 		{
 			redirect('./');
 		}
-		else
+
+		$user = $user[0];
+
+		if ($cost > $user["cash"])
 		{
-			$portfolio = CS50::query("SELECT * FROM portfolios WHERE id = ? AND user_id = ?", $_GET["id"], $_SESSION["cs673_id"]);
-			if (count($portfolio) == 1)
-			{
-				$portfolio = $portfolio[0];
-				$tickers = CS50::query("SELECT * FROM tickers WHERE portfolio_id = ?", $portfolio["id"]);
-
-				$output = [
-					"portfolio" => $portfolio,
-					"tickers" => $tickers,
-					"title" => "Holdings",
-					"subtitle" => $portfolio["name"]
-				];
-
-				render("portfolio.php", $output);
-
-			}
-			else
-			{
-				redirect('./');
-			}
+			$output["errors"] = ["This will cost $ {$cost}. You only have {$user['cash']} available to spend!"];
+			render('portfolio.php', $output);
 		}
+
+		$holding = CS50::query("SELECT * FROM tickers WHERE symbol = ?", $stock["ticker"]);
+
+		if (count($holding) > 0)
+		{
+			$output["errors"] = ["You already own {$stock["ticker"]}! Currently, you cannot purchase more without first selling. Sorry!"];
+			render('portfolio.php', $output);
+		}
+
+		$update = CS50::query("INSERT INTO tickers (symbol, name, exchange, shares, price, portfolio_id) VALUES (?, ?, ?, ?, ?, ?)", $stock["ticker"], $stock["name"], $exchanges[strtolower($stock["exchange"])], $_POST["shares"], $stock["price"], $_GET["id"]);
+		if (count($update) != 1)
+		{
+			$output["errors"] = ["Something went wrong - please try again."];
+			render('portfolio.php', $output);
+		}
+
+		$update = CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?", $cost, $_SESSION["cs673_id"]);
+		if (count($update) != 1)
+		{
+			$output["errors"] = ["Something went wrong - please try again."];
+			render('portfolio.php', $output);
+		}
+
+		redirect("./portfolio.php?id={$_GET["id"]}");
+
+
 	}
--- views/portfolio.php
+++ views/portfolio.php
@@ -7,6 +7,33 @@
 
 <hr />
 
+<div class="container-fluid">
+	<table class="table table-hover container-fluid" style=" text-align: center ">
+		<tr>
+			<th class="text-center">ID</th>
+			<th class="text-center">Symbol</th>
+			<th class="text-center">Name</th>
+			<th class="text-center">Exchange</th>
+			<th class="text-center">Shares</th>
+			<th class="text-center">Purchase Price</th>
+			<th class="text-center">Current Price</th>
+			<th class="text-center">Purchased On</th>
+		</tr>
+		<?php foreach($tickers as $ticker) { ?>
+			<tr>
+				<td><?= $ticker["id"] ?> </td>
+				<td><?= $ticker["symbol"] ?> </td>
+				<td><?= $ticker["name"] ?> </td>
+				<td><?= $ticker["exchange"] ?> </td>
+				<td><?= $ticker["shares"] ?> </td>
+				<td><?= $ticker["price"] ?> </td>
+				<td></td>
+				<td><?= $ticker["created_at"] ?></td>
+			</tr>
+		<?php } ?>
+	</table>
+</div>
+
 <script type="text/javascript">
 
 // On click, show modal.
@@ -23,9 +50,21 @@ $('#buyShares').on('shown.bs.modal', function () {
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title" id="myModalLabel">Buy Shares</h4>
 			</div>
-			<form method="post" action="portfolio.php">
+			<form method="post" action="portfolio.php?id=<?= $_GET["id"] ?>">
 				<div class="modal-body">
-					<input type="text" class="form-control" id="name" name="name" placeholder="My First Portfolio">
+					<input type="text" class="form-control" id="ticker" name="ticker" placeholder="AAPL">
+					<br />
+					<input type="number" class="form-control" id="shares" name="shares" placeholder="20">
+					<br />
+					<input type="number" class="form-control" id="price" name="price" placeholder="135.72">
+					<br />
+					<label for="select" class="text-left">Exchange</label>
+					<select class="form-control" name="exchange">
+						<option value="">(search all)</option>
+						<?php foreach($exchanges as $key => $exchange) { ?>
+						<option value="<?= $key; ?>"><?= $exchange; ?></option>
+						<?php } ?>
+					</select>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
