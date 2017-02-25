8b4fb2ab205b5ab48fa1f8b5a2efeb4f6433efbb
ananyap898 <ananya.himi122@gmail.com>
2017-02-24 22:58:22 -0500

    included cash(balance) column

--- database/migrations/02_create_portfolios_table.sql
+++ database/migrations/02_create_portfolios_table.sql
@@ -11,6 +11,9 @@ CREATE TABLE IF NOT EXISTS portfolios (
 	-- Foreign Keys
 	user_id INT UNSIGNED NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES users(id),
+	
+	-- Cash
+	balance DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
 
 	-- Timestamps
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
