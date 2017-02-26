fa137b1e9d6f166ded5618d462d7e5bd7d209ffb
ananyap898 <ananya.himi122@gmail.com>
2017-02-26 16:14:10 -0500

    reverted the change

--- public/portfolio.php
+++ public/portfolio.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Initializing the ini file.
-	$ini_array = parse_ini_file("../../portfolio.ini");
+	//$ini_array = parse_ini_file("../portfolio.ini");
 
 	// configuration
 	require("../includes/config.php");
@@ -12,9 +12,11 @@
 		redirect('./');
 	}
 
-	//$portfolio = CS50::query("SELECT * FROM portfolios WHERE id = ? AND user_id = ?", $_GET["id"], $_SESSION["cs673_id"]);
+	$portfolio = CS50::query("SELECT * FROM portfolios WHERE id = ? AND user_id = ?", $_GET["id"], $_SESSION["cs673_id"]);
+	
 	// Review this code. I am getting the select query from ini file.
-        $portfolio = CS50::query(.$ini_array['SELECT_PORTFOLIO'])
+        //$portfolio = CS50::query(.$ini_array['SELECT_PORTFOLIO'])
+	
 	if (count($portfolio) != 1)
 	{
 		redirect('./');
