c986418584267f0cb4ede5f4be5b7c370dabf2c2
Jay Ravaliya <jayrav13@gmail.com>
2017-03-02 19:38:39 -0500

    Helper function from StackOverflow dumps arrays to csv

--- includes/helpers.php
+++ includes/helpers.php
@@ -117,4 +117,23 @@
         }
     }
 
+    // http://stackoverflow.com/questions/16251625/how-to-create-and-download-a-csv-file-from-php-script
+    function array_to_csv_download($array, $filename = "export.csv", $delimiter=";") {
+        // open raw memory as file so no temp files needed, you might run out of memory though
+        $f = fopen('php://memory', 'w'); 
+        // loop over the input array
+        foreach ($array as $line) { 
+            // generate csv lines from the inner arrays
+            fputcsv($f, $line, $delimiter); 
+        }
+        // reset the file pointer to the start of the file
+        fseek($f, 0);
+        // tell the browser it's going to be a csv file
+        header('Content-Type: application/csv');
+        // tell the browser we want to save it instead of displaying it
+        header('Content-Disposition: attachment; filename="'.$filename.'";');
+        // make php send the generated csv lines to the browser
+        fpassthru($f);
+    }
+
 ?>
